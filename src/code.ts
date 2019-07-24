686c4085a0f85010d4aeb7a83113fb5faa5a8245
Luciano S. Junior <luciano.ssj@hotmail.com>
2019-07-24 12:14:24 -0300

    Testing some observables

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,32 @@
-import * as Rx from "rxjs/Observable";
+import { Observable } from "rxjs";
 
-console.log(Rx);
+var observable = Observable.create(function subscribe(observer:any) {
+    
+    try{
+
+        observer.next('Hey guys!');
+        observer.next('How are you?');
+
+        observer.complete();
+
+        observer.next('This will not send');
+
+    } catch(err) {
+
+        observer.error(err);
+
+    }
+
+});
+
+observable.subscribe(
+    (x:any) => addItem(x),
+    (error:any) => addItem(error),
+    () => addItem('Completed'));
+
+function addItem(val:any){
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+}
